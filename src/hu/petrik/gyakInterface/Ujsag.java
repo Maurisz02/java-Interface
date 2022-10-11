0c53d455043777204787a144220786318f19602f
Maurisz <p2002adam@gmail.com>
2022-10-11 14:47:10 +0200

    Ujsag class

---
+++ src/hu/petrik/gyakInterface/Ujsag.java
@@ -0,0 +1,26 @@
+package hu.petrik.gyakInterface;
+
+
+public class Ujsag implements Kolcsonozheto{
+
+    private String cim;
+    private int kiadasEv;
+    private int kiadasHet;
+
+    public Ujsag(String cim, int kiadasEv, int kiadasHet) {
+        this.cim = cim;
+        this.kiadasEv = kiadasEv;
+        this.kiadasHet = kiadasHet;
+    }
+
+
+    @Override
+    public String megjelenitendoNev() {
+        return String.format("%s %d/%d", this.cim,this.kiadasEv,this.kiadasHet);
+    }
+
+    @Override
+    public int meddigKolcsonozheto() {
+        return 0;
+    }
+}
